93e164baaff61d11fff5586a382effe31968cf48
nicocac <nicocac@gmail.com>
2016-05-28 22:10:35 -0300

    Carga de proyecto

---
+++ Desarrollo/Proyecto/src/PantallaCargarInsumo.java
@@ -0,0 +1,5 @@
+/**
+ * Created by OWNER on 5/28/2016.
+ */
+public class PantallaCargarInsumo {
+}
---
+++ Desarrollo/Proyecto/src/TipoInsumo.java
@@ -0,0 +1,35 @@
+/**
+ * Created by OWNER on 5/28/2016.
+ */
+public class TipoInsumo {
+
+    private String nombre;
+    private String descripcion;
+
+
+    public TipoInsumo(String nombre, String descripcion) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
+}
